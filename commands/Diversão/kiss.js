8d59952b562169117c57b4fb039bb8a8699ce781
matrizz <95687945+matrizz@users.noreply.github.com>
2025-07-16 19:12:01 -0300

    Update kiss.js

--- commands/Diversão/kiss.js
+++ commands/Diversão/kiss.js
@@ -16,19 +16,12 @@ module.exports = {
         let user = interaction.options.getUser("membro")
 
         var lista1 = [
-            'https://imgur.com/II1bakc.gif',
-            'https://imgur.com/MzAjNdv.gif',
-            'https://imgur.com/eKcWCgS.gif',
-            'https://imgur.com/3aX4Qq2.gif',
-            'https://imgur.com/uobBW9K.gif'
+            'https://imgur.com/gallery/mewooo-beautiful-1Tm0srp#ZWLDpHx',
+            
         ];
 
         var lista2 = [
-            'https://imgur.com/3jzT5g6.gif',
-            'https://imgur.com/VrETTlv.gif',
-            'https://imgur.com/FozOXkB.gif',
-            'https://imgur.com/7GhTplD.gif',
-            'https://imgur.com/B6UKulT.gif'
+           'https://imgur.com/gallery/pikachus-kiss-Sk17k1o',
         ];
 
         var random1 = lista1[Math.floor(Math.random() * lista1.length)];
